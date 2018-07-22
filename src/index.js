90f043de8fc0e8cf797f30b0fbb324a2b6db8070
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2018-07-22 22:21:02 +0100

    most works now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 (function (window) {
     'use strict';
 
-    var controllers = {}, modules = {}, services = {};
+    /**
+     * Registered entities.
+     */
+    var reg = {
+        controllers: {},
+        modules: {},
+        services: {}
+    };
+
+    /**
+     * Inner modules cache.
+     */
+    var modules;
 
     /**
      * Library's root object.
@@ -18,46 +30,50 @@
         version: '0.0.1',
 
         /**
-         * Registered services namespace
+         * Namespace of all registered and initialized services.
          */
         services: {},
 
         /**
-         * Creates and registers a new controller.
+         * Adds/Registers a new controller.
+         *
+         * If controller with such name already exists, it will be overridden.
          */
         addController: function (name, cb) {
-            if (typeof name === 'function') {
-                cb = name;
-                name = cb.name;
-            }
-            controllers[name] = cb;
+            addEntity(name, cb, 'controller', reg.controllers);
         },
 
         /**
-         * Creates and registers a new service.
+         * Adds/Registers a new service.
+         *
+         * If service with such name already exists, it will be overridden.
          */
         addService: function (name, cb) {
-            if (typeof name === 'function') {
-                cb = name;
-                name = cb.name;
-            }
-            services[name] = cb;
+            addEntity(name, cb, 'service', reg.services);
         },
 
         /**
          * Creates and registers a new module.
+         *
+         * If module with such name already exists, it will be overridden.
          */
         addModule: function (name, cb) {
-            if (typeof name === 'function') {
-                cb = name;
-                name = cb.name;
-            }
-            modules[name] = cb;
+            addEntity(name, cb, 'module', reg.modules);
         }
     };
 
-    function EController() {
-        /* for later, to be used only when finding one  */
+    function addEntity(name, cb, entity, obj) {
+        if (typeof name === 'function') {
+            cb = name;
+            name = cb.name;
+        }
+        if (!name || typeof name !== 'string') {
+            throw new TypeError('Invalid ' + entity + ' name specified. A non-empty string is required.');
+        }
+        if (typeof cb !== 'function') {
+            throw new TypeError('Missing function for controller ' + name + '.');
+        }
+        obj[name] = cb;
     }
 
     window.excellent = root;
@@ -72,14 +88,16 @@
     var find = document.querySelectorAll.bind(document);
 
     function initServices() {
-        for (var a in services) {
-            root.services[a] = services[a]();
+        root.services = {};
+        for (var a in reg.services) {
+            root.services[a] = reg.services[a]();
         }
     }
 
     function initModules() {
-        for (var a in modules) {
-            modules[a] = modules[a](); // overriding, for now
+        modules = {};
+        for (var a in reg.modules) {
+            modules[a] = reg.modules[a]();
         }
     }
 
@@ -102,12 +120,9 @@
             if (!name) {
                 throw new Error('Controller name is missing.');
             }
-            if (name in controllers) {
-                var cb = controllers[name];
-                if (typeof cb !== 'function') {
-                    throw new Error('Invalid controller ' + JSON.stringify(name) + ' type. Must be a function.');
-                }
-                cb();
+            if (name in reg.controllers) {
+                var cb = reg.controllers[name];
+                cb(new EController(e));
             } else {
                 throw new Error('Controller ' + JSON.stringify(name) + ' not found');
             }
@@ -128,6 +143,86 @@
         // Eureka! Simple Send/onReceive protocol, plus an easy `find(search)` method for each controller,
         // where `search` would be the standard DOMSearch filter.
     }
+
+    function EController(node) {
+
+        /**
+         * Source DOM element that uses this controller.
+         */
+        this.node = node;
+
+        this.children = {
+            find: function (criteria) {
+
+            },
+            findOne: function (criteria) {
+
+            }
+        };
+    }
+
+    /**
+     * Searches for all matching elements that have controllers,
+     * and returns the list of controllers.
+     *
+     * @param {String} criteria
+     *
+     * @returns {Array<EController>}
+     */
+    EController.prototype.find = function (criteria) {
+
+    };
+
+    /**
+     * Searches for exactly a single matching element that has a controller,
+     * and returns it.
+     *
+     * If no matching element found, or more than one found, it throws an error.
+     *
+     * @param {String} criteria
+     *
+     * @returns {EController}
+     */
+    EController.prototype.findOne = function (criteria) {
+
+    };
+
+    /**
+     * Synchronously sends data into method onReceive and returns the response,
+     * if the method exists. If onReceive handler does not exist, the method
+     * will do nothing, and return `undefined`.
+     *
+     * @param {} data
+     * Any type of data.
+     *
+     * @returns {}
+     * Whatever method onReceive returns.
+     */
+    EController.prototype.send = function (data) {
+        if (typeof this.onReceive === 'function') {
+            return this.onReceive(data, this);
+        }
+    };
+
+    /**
+     * Asynchronously sends data into method onReceive, and if the callback specified
+     * - calls it with the response.
+     *
+     * @param data
+     * @param {Function} [cb]
+     * Optional callback to receive the response from method onReceive.
+     */
+    EController.prototype.post = function (data, cb) {
+        var self = this;
+        setTimeout(function () {
+            if (typeof self.onReceive === 'function') {
+                var response = self.onReceive(data, self);
+                if (typeof cb === 'function') {
+                    cb(response);
+                }
+            }
+        });
+    };
 
     initRoot();
 
