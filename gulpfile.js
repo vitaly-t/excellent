8f2a0c580f16f7f8c44fb8c41af7cb319a950f1e
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2018-07-28 18:23:37 +0100

    integrating gulp.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const eslint = require('gulp-eslint');
+const rename = require('gulp-rename');
+const insert = require('gulp-insert');
+
+const SOURCE = './src/excellent.js';
+const DEST = 'excellent.min.js';
+
+const version = require('./package.json').version;
+
+const copyright = `/**
+ * Excellent.js v${version}
+ * Copyright 2018 Vitaly Tomilov
+ * Released under the MIT License
+ * https://github.com/vitaly-t/excellent
+ */
+`;
+
+gulp.task('lint', () => {
+    return gulp.src(SOURCE)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], () => {
+});
+
+function patch(content) {
+    return copyright + content.replace(/<version>/, version);
+}
+
+gulp.task('build', () => {
+    return gulp.src(SOURCE)
+        .pipe(uglify())
+        .pipe(rename(DEST))
+        .pipe(insert.transform(patch))
+        .pipe(gulp.dest('./src'));
+});
+
+gulp.task('default', ['lint', 'test', 'build']);
