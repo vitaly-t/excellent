9a447a4064282d1bb78cef9168480893fc3ca98c
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2018-08-01 01:22:38 +0100

    fixing travis ci config

--- test/header.js
+++ test/header.js
@@ -1,8 +1,13 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 
+const puppeteerOptions = {
+    args: ['--no-sandbox'], // this is to make puppeteer work on Travis CI
+    headless: true
+};
+
 async function createTest(file) {
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch(puppeteerOptions);
     const page = await browser.newPage();
     await page.goto(`file://${path.join(__dirname, file)}`);
     await page.addScriptTag({path: 'src/excellent.js'});
